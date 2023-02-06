99c8fbccbc61db8365b2c0f30e11ad6f334871a0
18137036 <9wjdgh9@naver.com>
2023-02-06 23:03:08 +0900

    design : Mainbanner component css 및 이미지 변경

--- src/components/Mainbanner.js
+++ src/components/Mainbanner.js
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 
 const MainbannerWrapper = styled.div`
     width: 100%;
-    height: 750px;
+    height: 600px;
     position: relative;
-    background-image : url('images/img9.jpg');
+    background-image : url('images/banner.jpg');
     background-repeat: no-repeat;
     background-size : cover;
     background-position: center;
