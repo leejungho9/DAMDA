f03265c8ce085414b409c027acf82c22887cc89e
18137036 <9wjdgh9@naver.com>
2023-02-07 01:12:30 +0900

    design : Advertisement component 이미지 분할 및 css 수정

--- src/components/Advertisement.js
+++ src/components/Advertisement.js
@@ -8,21 +8,64 @@ const AdvertisementWrapper = styled.div`
   margin-top: 140px;
 `;
 
-const AdvertisementImage = styled.div`
-  background-image: url("images/img5.jpg");
-  background-repeat:  no-repeat;
+const AdvertisementBg = styled.div`
   width: 100%;
   height: 200px;
-  background-repeat: no-repeat;
-  background-size : cover;
-  background-position: center;
+  background-color: #f6f6f6;
+  position: relative;
+`;
+
+const AdvertisementContainer = styled.div`
+  position: relative;
+  height: 100%;
+  margin: 0 auto;
+  max-width: 1250px;
+
 `
 
+const AdvertisementText = styled.div`
+    background-image: url(images/event3.png);
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center;
+    width: 350px;
+    height: 200px;
+    position: absolute;
+    left: 50px;
+`;
+
+const AdvertisementButton = styled.div`
+  background-image: url(images/event2.png);
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  width: 130px;
+  height: 200px;
+  position: absolute;
+  left: 430px;
+`
+
+const AdvertisementCoupon = styled.div`
+  background-image: url(images/event1.png);
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  width: 400px;
+  height: 200px;
+  position: absolute;
+  right: 0;
+`
 
 function Advertisement(props) {
   return (
     <AdvertisementWrapper>
-      <AdvertisementImage/>
+      <AdvertisementBg>
+        <AdvertisementContainer>
+          <AdvertisementText />
+          <AdvertisementButton />
+          <AdvertisementCoupon />
+        </AdvertisementContainer>
+      </AdvertisementBg>
     </AdvertisementWrapper>
   );
 }
