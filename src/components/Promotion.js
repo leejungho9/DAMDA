772ddf488e3b4892b4f6c1d5080169c8dad7fa99
18137036 <9wjdgh9@naver.com>
2023-02-07 19:00:47 +0900

    feat : Promotion component data.json 값으로 변경

--- src/components/Promotion.js
+++ src/components/Promotion.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-
+import data from "../assets/data/PromotionData.json";
 const PrmotionWrapper = styled.div`
   width: 100%;
   margin-top: 120px;
@@ -14,7 +14,7 @@ const PromotionMainTitle = styled.h3`
 const PrmotionMainImage = styled.div`
   width: 850px;
   height: 600px;
-  background-image: url("images/promotion_main.jpg");
+  background-image: url(${(props) => props.src});
   background-repeat: no-repeat;
   background-position: center;
 `;
@@ -58,7 +58,7 @@ const ItemContainer = styled.div`
 const ItemImage = styled.div`
   width: 220px;
   height: 220px;
-  background-image: url("images/promotion_sub.jpg");
+  background-image: url(${(props) => props.src});
   background-repeat: no-repeat;
 `;
 const ItemInfo = styled.div`
@@ -68,24 +68,24 @@ const ItemInfo = styled.div`
   justify-content: flex-end;
 `;
 const ItemCompany = styled.p`
-  font-family: 'LINESeedKR-Bd';
+  font-family: "LINESeedKR-Bd";
   font-size: 17px;
   margin-bottom: 30px;
 `;
 
 const ItemName = styled.p`
-  font-family: 'LINESeedKR-Rg';
+  font-family: "LINESeedKR-Rg";
   font-size: 17px;
 `;
 
 const ItemDiscount = styled.p`
-  font-family: 'LINESeedKR-Rg';
+  font-family: "LINESeedKR-Rg";
   font-size: 17px;
-  margin : 10px 0;
+  margin: 10px 0;
 `;
 
 const ItemPrice = styled.p`
-  font-family: 'LINESeedKR-Bd';
+  font-family: "LINESeedKR-Bd";
   font-size: 17px;
   color: #f28b39;
 `;
@@ -94,26 +94,30 @@ function Promotion(props) {
   return (
     <PrmotionWrapper>
       <PromotionMainTitle>PROMOTION</PromotionMainTitle>
-      <PromotionContainer>
-        <PrmotionMainImage />
-        <PromotionInfo>
-          <PromotionTitle>다가오는 추석, 선물 찾고계세요?</PromotionTitle>
-          <PromotionDate>2023.4.2-5.2</PromotionDate>
-          <PromotionDes>
-            단 한달 동안 진행하는 종려나무숲 선물패키지 이벤트입니다. 사랑하는 가족, 친구, 친지 마음을 담은 특별한 선물을 담다에서 준비하세요.
-          </PromotionDes>
-
-          <ItemContainer>
-            <ItemImage />
-            <ItemInfo>
-              <ItemCompany>허니순</ItemCompany>
-              <ItemName>꿀진담 2종 세트</ItemName>
-              <ItemDiscount>50%</ItemDiscount>
-              <ItemPrice>100,000</ItemPrice>
-            </ItemInfo>
-          </ItemContainer>
-        </PromotionInfo>
-      </PromotionContainer>
+      {data.data.map((el) => {
+        let buyPrice = (el.price * (el.discount / 100))
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        return (
+          <PromotionContainer key={el.p_id}>
+            <PrmotionMainImage src={el.url}/>
+            <PromotionInfo>
+              <PromotionTitle>{el.p_title}</PromotionTitle>
+              <PromotionDate>{el.p_start_date}</PromotionDate>
+              <PromotionDes>{el.p_des}</PromotionDes>
+              <ItemContainer>
+                <ItemImage src={el.sub_url} />
+                <ItemInfo>
+                  <ItemCompany>{el.company}</ItemCompany>
+                  <ItemName>{el.item_title}</ItemName>
+                  <ItemDiscount>{el.discount}%</ItemDiscount>
+                  <ItemPrice>{buyPrice}</ItemPrice>
+                </ItemInfo>
+              </ItemContainer>
+            </PromotionInfo>
+          </PromotionContainer>
+        );
+      })}
     </PrmotionWrapper>
   );
 }
