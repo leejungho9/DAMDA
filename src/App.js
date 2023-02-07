52a5e6b39589577332d7ad927affc7caefbfc985
jetty8013 <jetty8013@gmail.com>
2023-02-08 00:31:18 +0900

    feat : Bestreview component html, css 구현

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BestSeller from "./components/Bestseller";
 import Promotion from "./components/Promotion";
 import Advertisement from "./components/Advertisement";
 import Brand from "./components/Brand";
+import BestReview from "./components/Bestreview";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <BestSeller />
       <Advertisement/>
       <Promotion />
+      <BestReview />
       <Brand/>
     </>
   );
