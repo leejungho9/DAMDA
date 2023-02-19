363a530fd0e05827e2037a4563003ff9e949f95c
18137036 <9wjdgh9@naver.com>
2023-02-20 00:02:39 +0900

    feat(App) : Route 경로 shop detail 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Main from "./pages/Main";
 import Login from "./pages/Login";
 import Shop from "./pages/Shop";
 import Footer from "./components/Footer";
+import ShopDetail from "./pages/ShopDetail";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Main />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/shop" element={<Shop />}></Route>
+          <Route path="/shop/:id" element={<ShopDetail />}></Route>
         </Routes>
         <Footer/>
       </BrowserRouter>
