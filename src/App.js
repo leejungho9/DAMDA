375021c53b8e9b55e1427aae780b5a43dd1c36d9
18137036 <9wjdgh9@naver.com>
2023-03-27 04:12:06 +0900

    remove : 불필요한 코드 제거

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Main from "./pages/Main";
 import Login from "./pages/Login";
 import Shop from "./pages/Shop";
 import Footer from "./components/Footer";
 import ShopDetail from "./pages/ShopDetail";
-import { useEffect } from "react";
-import { analytics } from "./Firebase";
-
 
 function App() {
-  useEffect(()=> {
-    console.log(analytics)
-  })
   return (
     <>
       <BrowserRouter>
@@ -24,7 +18,7 @@ function App() {
           <Route path="/shop" element={<Shop />}></Route>
           <Route path="/shop/:id" element={<ShopDetail />}></Route>
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </>
   );
