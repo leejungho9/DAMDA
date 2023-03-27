446983c9ae7ee52a7c68f755520be79c7df0f6dc
18137036 <9wjdgh9@naver.com>
2023-03-27 12:39:53 +0900

    update : Firebase DatabaseURl 변수명 변경

--- src/Firebase.js
+++ src/Firebase.js
@@ -8,7 +8,7 @@ import { getDatabase } from "firebase/database";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_APIKEY,
-  databaseURL: process.env.REACT_APP_DATABASE,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
