d8ca6de91c554fef5e1056aca5becfb59c9446b3
18137036 <9wjdgh9@naver.com>
2023-04-08 03:43:10 +0900

    design : BestDetailReview Component 생성

---
+++ src/components/BestDetailReview.js
@@ -0,0 +1,110 @@
+import React from "react";
+import styled from "styled-components";
+import LineBar from "./BorderBar";
+
+const ReviewWrapper = styled.div`
+  margin-top: 105px;
+`;
+const ReviewContainer = styled.div`
+  margin-bottom: 45px;
+`;
+const ReviewTextArea = styled.div`
+  margin-bottom: 28px;
+  display: flex;
+`;
+const ReviewTitle = styled.h3`
+  font-size: 18px;
+  margin-right: 50px;
+  font-family: "LINESeedKR-Rg";
+`;
+const ReviewScope = styled.span`
+  font-size: 18px;
+  font-family: "LINESeedKR-Rg";
+`;
+const ReviewContentWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const ReviewContent = styled.div`
+  display: flex;
+  width: 650px;
+`;
+
+const ReviewContentImageBox = styled.div`
+  width: 100px;
+  height: 100px;
+  margin-right: 70px;
+`;
+const ReviewContentImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+const ReviewContentInfo = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+`;
+const ReviewContentScope = styled.span`
+  margin-top: 15px;
+  font-size: 16px;
+  font-family: "LINESeedKR-Rg";
+`;
+const ReviewContentText = styled.span`
+  margin-top: 20px;
+  font-size: 16px;
+  font-family: "LINESeedKR-Rg";
+`;
+
+const BorderBar = styled(LineBar)`
+  width: 100%;
+  margin-top: 15px;
+  margin-bottom: 35px;
+`;
+
+const BestDetailReview = ({ item }) => {
+  console.log(item);
+  return (
+    <ReviewWrapper>
+      <ReviewContainer>
+        <ReviewTextArea>
+          <ReviewTitle>BEST 고객리뷰</ReviewTitle>
+          <ReviewScope> {item.score} / 5.0</ReviewScope>
+        </ReviewTextArea>
+        <ReviewContentWrapper>
+          {/* 반복돼야함 */}
+          <ReviewContent>
+            <ReviewContentImageBox>
+              <ReviewContentImage
+                src={`${process.env.PUBLIC_URL}/${item.url}`}
+              />
+            </ReviewContentImageBox>
+            <ReviewContentInfo>
+              <ReviewContentScope>4.2 / 5.0</ReviewContentScope>
+              <ReviewContentText>
+                선물용으로 샀는데 너무 만족합니다.
+              </ReviewContentText>
+            </ReviewContentInfo>
+          </ReviewContent>
+          {/* 반복돼야함 */}
+          <ReviewContent>
+            <ReviewContentImageBox>
+              <ReviewContentImage
+                src={`${process.env.PUBLIC_URL}/${item.url}`}
+              />
+            </ReviewContentImageBox>
+            <ReviewContentInfo>
+              <ReviewContentScope>4.2 / 5.0</ReviewContentScope>
+              <ReviewContentText>
+                선물용으로 샀는데 너무 만족합니다.
+              </ReviewContentText>
+            </ReviewContentInfo>
+          </ReviewContent>
+        </ReviewContentWrapper>
+      </ReviewContainer>
+      <BorderBar />
+    </ReviewWrapper>
+  );
+};
+
+export default BestDetailReview;
