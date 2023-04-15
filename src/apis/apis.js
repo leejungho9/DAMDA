4ebbeca8709ec7947421c4ac27696a2ef3039775
leejungho9 <9wjdgh9@naver.com>
2023-04-15 22:33:04 +0900

    :sparkles: [Feat] api 통신 코드 작성

---
+++ src/apis/apis.js
@@ -0,0 +1,29 @@
+import db from "../Firebase";
+import { ref, get } from "firebase/database";
+
+export const getProducts = async () => {
+  const productsRef = ref(db, "products");
+  const snapshot = await get(productsRef, "value");
+
+  if (!snapshot.exists()) {
+    throw new Error("No products found");
+  }
+  const products = [];
+  snapshot.forEach((childSnapshot) => {
+    const product = childSnapshot.val();
+    products.push(product);
+  });
+  return products;
+};
+
+export const getDetailProducts = async (id) => {
+  const productsRef = ref(db, `detail/${id}`);
+  const snapshot = await get(productsRef);
+
+  if (!snapshot.exists()) {
+    throw new Error("상품의 상세페이지를 불러올 수 없습니다.");
+  }
+
+  const data = snapshot.val();
+  return data;
+};
