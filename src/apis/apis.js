9bd79ebaa3d906b76698bb9cc0be88e06e6360d3
leejungho9 <9wjdgh9@naver.com>
2023-04-16 23:41:41 +0900

    :sparkles: [Feat] CartItem get 요청 추가

--- src/apis/apis.js
+++ src/apis/apis.js
@@ -27,3 +27,18 @@ export const getDetailProducts = async (id) => {
   const data = snapshot.val();
   return data;
 };
+
+export const getCartItem = async (id) => {
+  const productsRef = ref(db, "cart_items/U01");
+  const snapshot = await get(productsRef);
+
+  if (!snapshot.exists()) {
+    throw new Error("장바구니 정보를 불러올 수 없습니다.");
+  }
+  const cartItems = [];
+  snapshot.forEach((childSnapshot) => {
+    const product = childSnapshot.val();
+    cartItems.push(product);
+  });
+  return cartItems;
+};
