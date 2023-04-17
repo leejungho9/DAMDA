045c0c83f9fcc7f2eaf279f178059247274b2726
leejungho9 <9wjdgh9@naver.com>
2023-04-17 21:44:42 +0900

    :sparkles: [Feat] 장바구니 정보 받아오는 액션 및 리듀서 정의

--- src/reducers/cartSlice.js
+++ src/reducers/cartSlice.js
@@ -1,38 +1,41 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { getCartItem } from "../apis/apis";
-import { useDispatch } from "react-redux";
-
-// initialState 초기값 db에서 받아오기
-
-// const initialState = [];
-
-// const fetchCartItems = async (dispatch) => {
-//   try {
-//     const cartItems = await getCartItem();
-//     initialState.push(cartItems);
-
-//     // 가져온 데이터를 초기값으로 설정
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
 
 //장바구니에 대한 상태와 관련된 액션과 리듀서 정의
 let cartSlice = createSlice({
   name: "cartItem",
   initialState: [],
   reducers: {
-    // plusQuantity: (state, action) => {
-    //   const { pid, quantity } = action.payload;
-    //   const item = state.find((item) => item.pid === id);
-    //   if (item) {
-    //     item.quantity += quantity;
-    //   }
-    // },
+    setItems: (state, action) => {
+      return action.payload;
+    },
+    plusQuantity: (state, action) => {
+      const { pid } = action.payload;
+      const item = state.find((item) => item.pid === pid);
+      if (item) {
+        item.quantity += 1;
+      }
+    },
+
+    minusQuantity: (state, action) => {
+      const { pid } = action.payload;
+      const item = state.find((item) => item.pid === pid);
+      if (item) {
+        item.quantity -= 1;
+      }
+    },
   },
 });
 
-export let { addCart } = cartSlice.actions;
+export const fetchCartItems = () => async (dispatch) => {
+  try {
+    const cartItems = await getCartItem();
+    dispatch(cartSlice.actions.setItems(cartItems));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const { plusQuantity, minusQuantity } = cartSlice.actions;
 
 export default cartSlice.reducer;
-// export { fetchCartItems };
