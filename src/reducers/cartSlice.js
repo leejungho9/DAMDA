1f5d49b1d297bda89ada7bd5ceb6363d4db1fa46
leejungho9 <9wjdgh9@naver.com>
2023-04-20 21:16:08 +0900

    :fire: [Remove] fetchCartItems 함수 제거

--- src/reducers/cartSlice.js
+++ src/reducers/cartSlice.js
@@ -1,12 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getCartItem } from "../apis/apis";
 
 //장바구니에 대한 상태와 관련된 액션과 리듀서 정의
-let cartSlice = createSlice({
+const cartSlice = createSlice({
   name: "cartItem",
   initialState: [],
   reducers: {
-    setItems: (state, action) => {
+    setCartItems: (state, action) => {
       return action.payload;
     },
     plusQuantity: (state, action) => {
@@ -27,15 +26,6 @@ let cartSlice = createSlice({
   },
 });
 
-export const fetchCartItems = () => async (dispatch) => {
-  try {
-    const cartItems = await getCartItem();
-    dispatch(cartSlice.actions.setItems(cartItems));
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-export const { plusQuantity, minusQuantity } = cartSlice.actions;
+export const { setCartItems, plusQuantity, minusQuantity } = cartSlice.actions;
 
 export default cartSlice.reducer;
