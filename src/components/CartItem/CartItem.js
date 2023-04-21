f663b141647c1541d6d31a24d957d918ebc21a3b
leejungho9 <9wjdgh9@naver.com>
2023-04-22 03:05:37 +0900

    :fire: [Remove] getCartItem hook 제거

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,67 +1,72 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import QuantityCounts from "../Counts/QuantityCounts";
 import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { getCartItem } from "../../apis/apis";
-import { setCartItems } from "../../reducers/cartSlice";
-
+import { useDispatch } from "react-redux";
+import { removeCartItem } from "../../reducers/cartSlice";
+import { IoMdClose } from "react-icons/io";
 const ProductImg = styled.img`
   width: 68px;
   height: 68px;
   padding: 18px 0;
 `;
 
-const CartItem = () => {
+const CloseIcon = styled(IoMdClose)`
+  cursor: pointer;
+  &:hover {
+    color: #f28b39;
+  }
+`;
+
+const CartItem = ({ cartItems }) => {
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    const setCartItemsFunc = async () => {
-      try {
-        const cartItems = await getCartItem();
-        dispatch(setCartItems(cartItems));
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    setCartItemsFunc();
-  }, [dispatch]);
-
-  const cartItems = useSelector((state) => state.cartItems || []);
 
   const priceFormatting = (price) => {
     return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   };
+
+  const onRemoveCartItem = (pid) => {
+    dispatch(removeCartItem(pid));
+  };
+
   return (
-    <>
-      {cartItems.map((item) => (
-        <tr key={item.pid}>
-          <td className="t_1">
-            <input type="checkbox" />
-          </td>
-          <td className="t_2">
-            <Link to={`/shop/${item.pid}`}>
-              <ProductImg
-                src={`${process.env.PUBLIC_URL}/${item.url}`}
-                alt="장바구니 이미지"
-              />
-            </Link>
-          </td>
+    <tbody>
+      {cartItems.length !== 0 ? (
+        cartItems.map((item) => (
+          <tr key={item.pid}>
+            <td className="t_1">
+              <input type="checkbox" />
+            </td>
+            <td className="t_2">
+              <Link to={`/shop/${item.pid}`}>
+                <ProductImg
+                  src={`${process.env.PUBLIC_URL}/${item.url}`}
+                  alt="장바구니 이미지"
+                />
+              </Link>
+            </td>
 
-          <td className="t_3">
-            {item.company}
-            <br />
-            {item.name}
-          </td>
+            <td className="t_3">
+              {item.company}
+              <br />
+              {item.name}
+            </td>
 
-          <td className="t_4">
-            <QuantityCounts quantity={item.quantity} pid={item.pid} />
-          </td>
-          <td className="t_5">{priceFormatting(item.price)}</td>
-          <td className="t_6">X</td>
+            <td className="t_4">
+              <QuantityCounts quantity={item.quantity} pid={item.pid} />
+            </td>
+            <td className="t_5">{priceFormatting(item.price)}</td>
+            <td className="t_6" onClick={() => onRemoveCartItem(item.pid)}>
+              <CloseIcon />
+            </td>
+          </tr>
+        ))
+      ) : (
+        <tr>
+          <td colSpan="6">카트에 담긴 제품이 없습니다.</td>
         </tr>
-      ))}
-    </>
+      )}
+    </tbody>
   );
 };
 
