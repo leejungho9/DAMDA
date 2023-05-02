dc8aa1f16d6d97578fd865a22626e4668b8e6d0f
leejungho9 <9wjdgh9@naver.com>
2023-05-02 15:18:18 +0900

    :hammer: [Update] Firebase 초기화 코드 수정

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,13 +12,13 @@ const firebaseConfig = {
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MEASUREMENT_ID,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getAnalytics(app);
+const db = getDatabase(app);
 
 export default db;
