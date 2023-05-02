8a5e55b81463da68755247159780d0a158245668
leejungho9 <9wjdgh9@naver.com>
2023-05-02 10:19:13 +0900

    :lipstick: [Design] CartList Component 생성

---
+++ src/components/CartList/CartList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import CartItem from "../CartItem/CartItem";
+
+const CartList = ({ cartItems, orderNowMode }) => {
+  return cartItems.length !== 0 ? (
+    <tbody>
+      {cartItems.map((item) => (
+        <CartItem item={item} orderNowMode={orderNowMode} />
+      ))}
+    </tbody>
+  ) : (
+    <tr>
+      <td colSpan="6">카트에 담긴 제품이 없습니다.</td>
+    </tr>
+  );
+};
+
+export default CartList;
