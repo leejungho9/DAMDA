4148d8061ca0951160b04fb4656beb644819e3b9
leejungho9 <9wjdgh9@naver.com>
2023-05-10 12:41:34 +0900

    :hammer: [Update] db import 방식 수정

--- src/components/Promotion.js
+++ src/components/Promotion.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { ref, onValue } from "firebase/database";
-import db from "../Firebase";
+import { db } from "../Firebase";
 
 const PrmotionWrapper = styled.div`
   width: 100%;
