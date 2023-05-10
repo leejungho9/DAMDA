74742e003c39e4ec0bf24794dab1f6856906640d
leejungho9 <9wjdgh9@naver.com>
2023-05-10 12:29:11 +0900

    :sparkles: [Feat] Firebase auth 추가

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,5 +21,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
+const auth = getAuth(app);
 
-export default db;
+export { db, auth };
