165331a884f63e7da31c591bab56b80fc87a46f7
leejungho9 <9wjdgh9@naver.com>
2023-05-23 14:39:50 +0900

    :sparkles: userSlice 생성
    로그인 한 유저의 대한 상태와 관련도니 액션 및 리듀서 정의

---
+++ src/reducers/userSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// ! 로그인 한 유저에 대한 상태와 관련된 액션과 리듀서 정의
+const userSlice = createSlice({
+  name: "user",
+  initialState: { isLoggedIn: false, user: {} },
+  reducers: {
+    login: (state, action) => {
+      return { ...state, isLoggedIn: true, user: action.payload };
+    },
+    logout: (state) => {
+      state.isLogin = false;
+      state.user = {};
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+
+export default userSlice.reducer;
