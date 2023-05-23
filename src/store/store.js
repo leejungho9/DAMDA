02e593991f2c1d30d357a5dce0a4073427a5e510
leejungho9 <9wjdgh9@naver.com>
2023-05-23 14:29:55 +0900

    :zap: userRedoucer 추가

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../reducers/cartSlice";
+import userReducer from "../reducers/userSlice";
 
 const store = configureStore({
   reducer: {
     cartItems: cartReducer,
+    user: userReducer,
   },
 });
 export default store;
