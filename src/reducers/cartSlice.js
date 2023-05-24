4b6236e8711878b1e73086fd3fb7355727ad6908
leejungho9 <9wjdgh9@naver.com>
2023-05-24 13:19:24 +0900

    :sparkles: [Feat] 로그인 유저의 장바구니 데이터 가져오기

--- src/reducers/cartSlice.js
+++ src/reducers/cartSlice.js
@@ -1,5 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { push, query, ref, remove, set, get } from "firebase/database";
+import {
+  push,
+  query,
+  ref,
+  remove,
+  set,
+  get,
+  equalTo,
+  orderByChild,
+  update,
+} from "firebase/database";
 import { db } from "../Firebase";
 
 //! 장바구니에 대한 상태와 관련된 액션과 리듀서 정의
@@ -11,71 +21,62 @@ const cartSlice = createSlice({
       return action.payload;
     },
     addCartItem: (state, action) => {
-      const newCartItem = action.payload;
-      const cartItemsRef = ref(db, "cart_items/U01");
+      const { data, userId } = action.payload;
+      const cartItemsRef = ref(db, "cart_items/" + userId);
       const newCartRef = push(cartItemsRef);
-      set(newCartRef, newCartItem);
+      set(newCartRef, data);
     },
     plusQuantity: (state, action) => {
-      const { pid } = action.payload;
-      const item = state.find((item) => item.pid === pid);
-      if (item) {
-        item.quantity += 1;
-        const cartItemsRef = ref(db, "cart_items/U01");
-        const cartItemsQuery = query(cartItemsRef);
+      const { pid, userId } = action.payload;
+      // ! 장바구니 안에서 같은 pid 의 quantity 값 + 1
+      const items = state.find((item) => item.pid === pid);
+      if (items) {
+        items.quantity += 1;
+        // ! db 안에 pid 값 비교
+        const cartItemsRef = ref(db, "cart_items/" + userId);
+        const queryRef = query(cartItemsRef, orderByChild("pid"), equalTo(pid));
 
-        get(cartItemsQuery).then((snapshot) => {
-          const cartItems = snapshot.val();
-
-          for (const key in cartItems) {
-            if (cartItems[key].pid === pid) {
-              const cartItemRef = ref(db, `cart_items/U01/${key}`);
-              const newQuantity = cartItems[key].quantity + 1;
-              set(cartItemRef, { ...cartItems[key], quantity: newQuantity });
-              break;
-            }
+        get(queryRef).then((snapshot) => {
+          const cartItem = snapshot.val();
+          const key = Object.keys(cartItem);
+          if (cartItem) {
+            const ItemRef = ref(db, `cart_items/${userId}/${key}`);
+            update(ItemRef, { quantity: cartItem[key].quantity + 1 });
           }
         });
       }
     },
     minusQuantity: (state, action) => {
-      const { pid } = action.payload;
-      const item = state.find((item) => item.pid === pid);
-      if (item) {
-        item.quantity -= 1;
-        const cartItemsRef = ref(db, "cart_items/U01");
-        const cartItemsQuery = query(cartItemsRef);
+      const { pid, userId } = action.payload;
+      // ! 장바구니 안에서 같은 pid 의 quantity 값 - 1
+      const items = state.find((item) => item.pid === pid);
+      if (items) {
+        items.quantity -= 1;
+        // ! db 안에 pid 값 비교
+        const cartItemsRef = ref(db, "cart_items/" + userId);
+        const queryRef = query(cartItemsRef, orderByChild("pid"), equalTo(pid));
 
-        get(cartItemsQuery).then((snapshot) => {
-          const cartItems = snapshot.val();
-
-          for (const key in cartItems) {
-            if (cartItems[key].pid === pid) {
-              const cartItemRef = ref(db, `cart_items/U01/${key}`);
-              const newQuantity = cartItems[key].quantity - 1;
-              set(cartItemRef, { ...cartItems[key], quantity: newQuantity });
-              break;
-            }
+        get(queryRef).then((snapshot) => {
+          const cartItem = snapshot.val();
+          const key = Object.keys(cartItem);
+          if (cartItem) {
+            const ItemRef = ref(db, `cart_items/${userId}/${key}`);
+            update(ItemRef, { quantity: cartItem[key].quantity - 1 });
           }
         });
       }
     },
     removeCartItem: (state, action) => {
-      const pid = action.payload;
-      const cartItemsRef = ref(db, "cart_items/U01");
-      const cartItemsQuery = query(cartItemsRef);
+      const { pid, userId } = action.payload;
+      const cartItemsRef = ref(db, "cart_items/" + userId);
+      const queryRef = query(cartItemsRef, orderByChild("pid"), equalTo(pid));
 
-      get(cartItemsQuery).then((snapshot) => {
-        const cartItems = snapshot.val();
-        console.log(cartItems);
-        for (const key in cartItems) {
-          console.log(cartItems[key].pid);
-          console.log(cartItems[key].pid === pid);
-          if (cartItems[key].pid === pid) {
-            const cartItemRef = ref(db, `cart_items/U01/${key}`);
-            remove(cartItemRef);
-            break;
-          }
+      get(queryRef).then((snapshot) => {
+        const cartItem = snapshot.val();
+        const key = Object.keys(cartItem);
+        if (cartItem) {
+          const ItemRef = ref(db, `cart_items/${userId}/${key}`);
+          remove(ItemRef);
         }
       });
 
