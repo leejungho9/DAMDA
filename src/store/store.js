0cd5655d868a2740e6d09c350192449b02277260
leejungho9 <9wjdgh9@naver.com>
2023-05-28 03:10:41 +0900

    :sparkles: [Feat] wishReducer 추가

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../reducers/cartSlice";
 import userReducer from "../reducers/userSlice";
+import wishReducer from "../reducers/wishSlice";
 
 const store = configureStore({
   reducer: {
     cartItems: cartReducer,
+    wishItems: wishReducer,
     user: userReducer,
   },
 });
+
 export default store;
