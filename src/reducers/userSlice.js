ab67b6c0896af91653845b0bc991ef376e76e932
leejungho9 <9wjdgh9@naver.com>
2023-05-28 03:09:27 +0900

    :hammer: [Update] user login reducers 변경

--- src/reducers/userSlice.js
+++ src/reducers/userSlice.js
@@ -1,12 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
-
 // ! 로그인 한 유저에 대한 상태와 관련된 액션과 리듀서 정의
+
 const userSlice = createSlice({
   name: "user",
-  initialState: { isLoggedIn: false, user: {} },
+  initialState: {
+    isLoggedIn: false,
+    user: {},
+  },
   reducers: {
     login: (state, action) => {
-      return { ...state, isLoggedIn: true, user: action.payload };
+      state.isLoggedIn = true;
+      state.user = action.payload;
     },
     logout: (state) => {
       state.isLogin = false;
