83bf5c6334ed63cc34b2a2688bea9586af37808d
leejungho9 <9wjdgh9@naver.com>
2023-05-28 22:12:47 +0900

    :sparkles: [Feat] 현재 로그인 유저의 정보 저장

--- src/App.js
+++ src/App.js
@@ -11,8 +11,39 @@ import Order from "./pages/Order";
 import ScrollToTop from "./hooks/ScrollToTop";
 import Signup from "./pages/Signup";
 import Wish from "./pages/Wish";
+import { useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getUser } from "./apis/apis";
+import { useDispatch } from "react-redux";
+import { login } from "./reducers/userSlice";
 
 function App() {
+  // ! 현재 로그인한 사용자 정보 가져오기 (혹시 모르니 남겨두기)
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const CheckLoginUserInfo = async () => {
+      const auth = getAuth();
+
+      const getLoggedInUserId = new Promise((resolve, reject) => {
+        onAuthStateChanged(auth, (user) => {
+          if (user) {
+            resolve(user.uid);
+          } else {
+            reject(null);
+          }
+        });
+      });
+      if (getLoggedInUserId) {
+        const userId = await getLoggedInUserId;
+        const userInfo = await getUser(userId);
+        dispatch(login({ isLoggedIn: true, user: userInfo }));
+        return userInfo;
+      } else {
+        return undefined;
+      }
+    };
+    CheckLoginUserInfo();
+  }, []);
   return (
     <>
       <BrowserRouter>
