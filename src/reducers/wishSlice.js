7112836c5c8393310e2aea20c487c29299ba4079
leejungho9 <9wjdgh9@naver.com>
2023-05-28 22:31:22 +0900

    :sparkles: [Feat] wishSlice reducer 추가

--- src/reducers/wishSlice.js
+++ src/reducers/wishSlice.js
@@ -1,6 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { db } from "../Firebase";
-import { push, ref } from "firebase/database";
+import {
+  equalTo,
+  get,
+  orderByChild,
+  push,
+  query,
+  ref,
+  remove,
+} from "firebase/database";
 
 //! 관심상품에 대한 상태와 관련된 액션과 리듀서 정의
 const wishSlice = createSlice({
@@ -14,11 +22,28 @@ const wishSlice = createSlice({
       const { data, userId } = action.payload;
       const wishItemRef = ref(db, "wish_items/" + userId);
       push(wishItemRef, data);
-      // set(newWishRef, data);
+    },
+    removeWishItem: (state, action) => {
+      const { checkItemId, userId } = action.payload;
+      const wishItemRef = ref(db, "wish_items/" + userId);
+
+      checkItemId.forEach((pid) => {
+        const queryRef = query(wishItemRef, orderByChild("pid"), equalTo(pid));
+        get(queryRef).then((snapshot) => {
+          const wishItem = snapshot.val();
+          const key = Object.keys(wishItem);
+          if (wishItem) {
+            const wishRef = ref(db, `wish_items/${userId}/${key}`);
+            remove(wishRef);
+          }
+        });
+      });
+
+      return state.filter((pid) => !checkItemId.includes(pid));
     },
   },
 });
 
-export const { setWishItem, addWishItem } = wishSlice.actions;
+export const { setWishItem, addWishItem, removeWishItem } = wishSlice.actions;
 
 export default wishSlice.reducer;
