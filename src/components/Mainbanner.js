99c4d394639192bde59de324d55adb5215c9952e
leejungho9 <9wjdgh9@naver.com>
2023-06-01 15:16:19 +0900

    :lipstick: [Design] MainBannerWrapper 생성

--- src/components/Mainbanner.js
+++ src/components/Mainbanner.js
@@ -3,16 +3,23 @@ import styled from "styled-components";
 
 const MainbannerWrapper = styled.div`
   width: 100%;
+  background-color: #fbeece;
+`;
+const MainBannerImage = styled.div`
   height: 600px;
   position: relative;
   background-image: url(../images/banner.jpg);
   background-repeat: no-repeat;
-  background-size: cover;
+  background-size: contain;
   background-position: center;
 `;
 
 function Mainbanner(props) {
-  return <MainbannerWrapper />;
+  return (
+    <MainbannerWrapper>
+      <MainBannerImage />
+    </MainbannerWrapper>
+  );
 }
 
 export default Mainbanner;
