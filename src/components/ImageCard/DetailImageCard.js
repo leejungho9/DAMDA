5350de57ff6f254ed85921eb13847f90591c60d8
leejungho9 <9wjdgh9@naver.com>
2023-06-04 17:49:13 +0900

    :lipstick: [Design] DetailImageCard 컴포넌트 생성

---
+++ src/components/ImageCard/DetailImageCard.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+
+const ShopImageGalleryBox = styled.div`
+  display: flex;
+  width: 600px;
+  justify-content: flex-start;
+  margin-top: 23px;
+`;
+
+const ShopImage = styled.img`
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+  margin-right: 25px;
+
+  &:nth-last-of-type() {
+    margin-right: 0;
+  }
+`;
+
+function DetailImageCard({ detailImages }) {
+  return (
+    <ShopImageGalleryBox>
+      {detailImages.map((image) => {
+        return (
+          <ShopImage
+            alt="상품 디테일 이미지"
+            src={`${process.env.PUBLIC_URL}/${image}`}
+          />
+        );
+      })}
+    </ShopImageGalleryBox>
+  );
+}
+
+export default DetailImageCard;
