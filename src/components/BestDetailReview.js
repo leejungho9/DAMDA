6d16041e37e05a2583c3ccd6a370fd0004dba066
leejungho9 <9wjdgh9@naver.com>
2023-06-05 21:07:43 +0900

    :sparkles: [Feat] 리뷰 출력 및 작성 기능 구현

--- src/components/BestDetailReview.js
+++ src/components/BestDetailReview.js
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
+import { Rate } from "antd";
+import { addReview, getReviews } from "../apis/apis";
+import { useSelector } from "react-redux";
+import ReviewCarosule from "./Carousel/ReviewCarosule";
 import LineBar from "./BorderBar";
 
 const ReviewWrapper = styled.div`
@@ -11,6 +16,8 @@ const ReviewContainer = styled.div`
 const ReviewTextArea = styled.div`
   margin-bottom: 28px;
   display: flex;
+  position: relative;
+  align-items: center;
 `;
 const ReviewTitle = styled.h3`
   font-size: 18px;
@@ -21,86 +28,189 @@ const ReviewScope = styled.span`
   font-size: 18px;
   font-family: "LINESeedKR-Rg";
 `;
-const ReviewContentWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
+const ReviewContentWrapper = styled.div``;
+
+const BorderBar = styled(LineBar)`
+  width: 100%;
+  margin-bottom: 30px;
 `;
-const ReviewContent = styled.div`
-  display: flex;
+const ReviewWriteButton = styled.div`
+  position: absolute;
+  right: 0;
+  cursor: pointer;
+`;
+const ReviewContentInput = styled.textarea`
+  height: 150px;
+  width: 100%;
+  outline: none;
+  resize: none;
+  border: 1px solid #cdcdcd;
+  font-size: 15px;
+  padding: 10px;
+  box-sizing: border-box;
+  font-family: "LINESeedKR-Rg";
+`;
+const SubmitReviewButtonBox = styled.div`
+  position: relative;
+  padding: 10px 0;
+`;
+
+const SubmitReviewButton = styled.button`
+  border: none;
+  background: white;
+  border-radius: 10px;
+  font-size: 15px;
+  font-family: "LINESeedKR-Rg";
+  position: absolute;
+  right: 10px;
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const ReviewContentBox = styled.div`
+  display: flex !important;
   width: 650px;
 `;
 
-const ReviewContentImageBox = styled.div`
-  width: 100px;
-  height: 100px;
-  margin-right: 70px;
-`;
-const ReviewContentImage = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`;
-const ReviewContentInfo = styled.div`
-  display: flex;
-  align-items: flex-start;
-  flex-direction: column;
-`;
-const ReviewContentScope = styled.span`
-  margin-top: 15px;
-  font-size: 16px;
-  font-family: "LINESeedKR-Rg";
-`;
 const ReviewContentText = styled.span`
   margin-top: 20px;
   font-size: 16px;
   font-family: "LINESeedKR-Rg";
 `;
 
-const BorderBar = styled(LineBar)`
-  width: 100%;
-  margin-bottom: 100px;
-`;
+const BestDetailReview = ({
+  pid,
+  item,
+  reviewWriteMode,
+  setReviewWriteMode,
+  detailImages,
+}) => {
+  const [reviewScore, setReviewScore] = useState(2.5);
+  const [reviewContent, setReviewContent] = useState("");
+  const { user } = useSelector((state) => state.user);
+  const userId = user.userId;
+  const [isReviews, setReviews] = useState([]);
+  const [allScore, setAllScore] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const changeReviewHandelr = (event) => {
+    setReviewScore(event);
+  };
+  const changeReviewHandler = (event) => {
+    setReviewContent(event.target.value);
+  };
 
-const BestDetailReview = ({ item }) => {
+  const SubmitReviewHandler = async () => {
+    try {
+      await addReview(pid, reviewScore, reviewContent, userId, user.name);
+      setReviewContent("");
+      setReviewScore(2.5);
+      alert("리뷰가 정상적으로 작성됐습니다.");
+      setReviewWriteMode(false);
+
+      // ! 작성한 리뷰를 isReviews 배열에 추가
+      const newReview = {
+        reviewId: isReviews.length + 1,
+        userId: userId,
+        userName: user.name,
+        reviewScore: reviewScore,
+        reviewContent: reviewContent,
+      };
+      setReviews([...isReviews, newReview]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //! pid에 대한 리뷰 가져오기
+  useEffect(() => {
+    const fetchReviews = async () => {
+      try {
+        const reviews = await getReviews(pid);
+        setReviews(reviews);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchReviews();
+  }, [pid]);
+
+  //! pid에 대한 리뷰 가져오기
+  useEffect(() => {
+    const allScoreHandler = () => {
+      if (isReviews.length === 0) {
+        setAllScore(0);
+      } else {
+        const totalScore = isReviews.reduce(
+          (sum, review) => sum + review.reviewScore,
+          0
+        );
+        const averageScore = totalScore / isReviews.length;
+        setAllScore(Math.round(averageScore * 10) / 10);
+      }
+    };
+
+    allScoreHandler();
+  }, [isReviews]);
+
   return (
     <ReviewWrapper>
       <ReviewContainer>
         <ReviewTextArea>
           <ReviewTitle>BEST 고객리뷰</ReviewTitle>
-          <ReviewScope> {item.score} / 5.0</ReviewScope>
+          <ReviewScope> {allScore} / 5.0</ReviewScope>
+          <ReviewWriteButton>
+            {reviewWriteMode ? (
+              <AiOutlineMinus onClick={() => setReviewWriteMode(false)} />
+            ) : (
+              <AiOutlinePlus onClick={() => setReviewWriteMode(true)} />
+            )}
+          </ReviewWriteButton>
         </ReviewTextArea>
         <ReviewContentWrapper>
-          {/* 반복돼야함 */}
-          <ReviewContent>
-            <ReviewContentImageBox>
-              <ReviewContentImage
-                src={`${process.env.PUBLIC_URL}/${item.url}`}
-              />
-            </ReviewContentImageBox>
-            <ReviewContentInfo>
-              <ReviewContentScope>4.2 / 5.0</ReviewContentScope>
-              <ReviewContentText>
-                선물용으로 샀는데 너무 만족합니다.
-              </ReviewContentText>
-            </ReviewContentInfo>
-          </ReviewContent>
-          {/* 반복돼야함 */}
-          <ReviewContent>
-            <ReviewContentImageBox>
-              <ReviewContentImage
-                src={`${process.env.PUBLIC_URL}/${item.url}`}
-              />
-            </ReviewContentImageBox>
-            <ReviewContentInfo>
-              <ReviewContentScope>4.2 / 5.0</ReviewContentScope>
-              <ReviewContentText>
-                선물용으로 샀는데 너무 만족합니다.
-              </ReviewContentText>
-            </ReviewContentInfo>
-          </ReviewContent>
+          {isReviews.length === 0 ? (
+            <ReviewContentBox>
+              <ReviewContentText> 작성된 리뷰가 없습니다.</ReviewContentText>
+            </ReviewContentBox>
+          ) : (
+            <ReviewCarosule
+              pid={pid}
+              isLoading={isLoading}
+              isReviews={isReviews}
+              detailImages={detailImages}
+              setReviews={setReviews}
+            />
+          )}
         </ReviewContentWrapper>
       </ReviewContainer>
       <BorderBar />
+      {reviewWriteMode && (
+        <ReviewContainer>
+          <ReviewTextArea>
+            <ReviewTitle>리뷰 작성하기</ReviewTitle>
+            <ReviewScope>
+              <Rate
+                allowHalf
+                defaultValue={2.5}
+                onChange={changeReviewHandelr}
+                value={reviewScore}
+              />
+            </ReviewScope>
+          </ReviewTextArea>
+          <ReviewContentInput
+            placeholder="내용을 입력해주세요."
+            value={reviewContent}
+            onChange={changeReviewHandler}
+          />
+          <SubmitReviewButtonBox>
+            <SubmitReviewButton onClick={SubmitReviewHandler}>
+              등록
+            </SubmitReviewButton>
+          </SubmitReviewButtonBox>
+        </ReviewContainer>
+      )}
     </ReviewWrapper>
   );
 };
