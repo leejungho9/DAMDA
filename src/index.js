20da9c1a85b62d2ff1408d8704472650deb19b72
leejungho9 <9wjdgh9@naver.com>
2023-08-07 18:12:31 +0900

    :fire: [Remove] 불필요한 import문 및 파일 제거

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store/store";
-// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
